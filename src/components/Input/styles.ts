6f19adc84476842c5ee0ba4ba7de3709db53d65d
marcos <marcos.rib.sousa@gmail.com>
2020-09-25 01:07:36 -0300

    created validation

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   height: 50px;
   border-radius: 10px;
@@ -16,6 +21,18 @@ export const Container = styled.div`
     margin-top: 8px;
   }
 
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
   input {
     flex: 1;
     background: transparent;
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErros from '../../utils/getValidationErros';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -11,32 +14,51 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const Signup: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: Object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No minimo 6 digitos'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErros(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="goBarber" />
-      <Form>
-        <h1>Faça seu cadastro</h1>
-        <Input icon={FiUser} name="nome" placeholder="Nome" />
-        <Input icon={FiMail} name="email" placeholder="E-email" />
-        <Input
-          icon={FiLock}
-          name="password"
-          type="password"
-          placeholder="password"
-        />
-        <Button type="submit">Entrar</Button>
-      </Form>
-      <a href="conta">
-        <FiArrowLeft />
-        Voltar para logon
-      </a>
-    </Content>
-  </Container>
-  )
-}
-
-);
+      <Background />
+      <Content>
+        <img src={logoImg} alt="goBarber" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input icon={FiUser} name="name" placeholder="Nome" />
+          <Input icon={FiMail} name="email" placeholder="E-email" />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+          <Button type="submit">Entrar</Button>
+        </Form>
+        <a href="conta">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default Signup;
